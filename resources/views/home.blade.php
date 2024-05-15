3e5c5e45e3db9f123225858500fe15b0bbfd53ab
Dilshan <tharindudilshan@gmail.com>
2024-05-15 15:05:48 +0530

    🚧recreated inquirie tbl

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -259,8 +259,21 @@
                                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                     </div>
                                                     <div class="modal-body">
-                                                        ...
-                                                    </div>
+                                                        <form>
+                                                          <div class="mb-3">
+                                                            <label for="recipient-name" class="col-form-label">Recipient:</label>
+                                                            <input type="text" class="form-control" id="recipient-name">
+                                                          </div>
+                                                          <div class="mb-3">
+                                                            <label for="recipient-name" class="col-form-label">Subject:</label>
+                                                            <input type="text" class="form-control" id="Subject-text">
+                                                          </div>
+                                                          <div class="mb-3">
+                                                            <label for="message-text" class="col-form-label">Message:</label>
+                                                            <textarea class="form-control" id="message-text"></textarea>
+                                                          </div>
+                                                        </form>
+                                                      </div>
                                                     <div class="modal-footer">
                                                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                                                         <button type="button" class="btn btn-primary">Response</button>
@@ -304,8 +317,21 @@
                                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                     </div>
                                                     <div class="modal-body">
-                                                        ...
-                                                    </div>
+                                                        <form>
+                                                          <div class="mb-3">
+                                                            <label for="recipient-name" class="col-form-label">Recipient:</label>
+                                                            <input type="text" class="form-control" id="recipient-name">
+                                                          </div>
+                                                          <div class="mb-3">
+                                                            <label for="recipient-name" class="col-form-label">Subject:</label>
+                                                            <input type="text" class="form-control" id="Subject-text">
+                                                          </div>
+                                                          <div class="mb-3">
+                                                            <label for="message-text" class="col-form-label">Message:</label>
+                                                            <textarea class="form-control" id="message-text"></textarea>
+                                                          </div>
+                                                        </form>
+                                                      </div>
                                                     <div class="modal-footer">
                                                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                                                         <button type="button" class="btn btn-primary">Response</button>
@@ -349,8 +375,21 @@
                                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                     </div>
                                                     <div class="modal-body">
-                                                        ...
-                                                    </div>
+                                                        <form>
+                                                          <div class="mb-3">
+                                                            <label for="recipient-name" class="col-form-label">Recipient:</label>
+                                                            <input type="text" class="form-control" id="recipient-name">
+                                                          </div>
+                                                          <div class="mb-3">
+                                                            <label for="recipient-name" class="col-form-label">Subject:</label>
+                                                            <input type="text" class="form-control" id="Subject-text">
+                                                          </div>
+                                                          <div class="mb-3">
+                                                            <label for="message-text" class="col-form-label">Message:</label>
+                                                            <textarea class="form-control" id="message-text"></textarea>
+                                                          </div>
+                                                        </form>
+                                                      </div>
                                                     <div class="modal-footer">
                                                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                                                         <button type="button" class="btn btn-primary">Response</button>
